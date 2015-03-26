aeccff27eb9d9a8fec4845e90a92d65022c68165
Darian <darian@Darians-MacBook.local>
2015-03-26 15:08:34 +0000

    Qualification phase
    Fixed turn function

--- Race/race.c
+++ Race/race.c
@@ -1,5 +1,3 @@
-// The turn function has to be improved.
-
 #include "math.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,21 +6,18 @@
 #define DESIRED_FRONT_DIST 32
 #define SPEED 30.0
 
-#define GAIN 0.01           
+#define GAIN 0.1           
 #define NEW_GAIN_MIN 0.3
-#define NEW_GAIN_MAX 0.98
+#define NEW_GAIN_MAX 0.9
 
 #define ROBOT_WIDTH 22.5
 #define WHEEL_DIAMETER 9.5
 #define TICKS_IN_METER 1146
 #define CENTIMETER 12.06
-#define UNIT CENTIMETER*60
-
-#define METER 1146
-#define SLOW_DIST METER*0.1
+#define UNIT CENTIMETER*60-10
 
 
-int ticks_left = 0, ticks_right = 0, orientation = 0, position = 1, i;
+int ticks_left = 0, ticks_right = 0, orientation = 0, position = 0, stop = 0, i;
 int frontLeftDist, frontRightDist, sideLeftDist, sideRightDist, USDist;
 double previous_cm_right = 0, previous_cm_left = 0, delta_angle = 0, current_angle = 0;
 double cm_right = 0, cm_left = 0, total_d_y = 0, total_d_x = 0;
@@ -73,7 +68,7 @@ void get_readings() {
 	get_front_ir_dists(&frontLeftDist, &frontRightDist);
 	get_side_ir_dists(&sideLeftDist, &sideRightDist);
 	USDist=get_us_dist();
-	printf("US: %d\n", USDist);
+	//printf("US: %d\n", USDist);
 }
 
 void angle_change() {
@@ -97,75 +92,40 @@ int new_speed(double target, double current) {
 	double difference = fabs(target-current);
 	double proportion = difference * GAIN;
 	if ( NEW_GAIN_MIN <= proportion && proportion <= NEW_GAIN_MAX) {
-		new_gain = 1 - proportion;
+		new_gain = proportion;
 	}
 	else 
 		if (proportion > NEW_GAIN_MAX) {
-			new_gain = 1 - NEW_GAIN_MAX;
+			new_gain = NEW_GAIN_MAX;
 		}
 		else
 		{
-			new_gain = 1 - NEW_GAIN_MIN;
+			new_gain = 1 - NEW_GAIN_MAX;
 		}
+	printf("new_gain: %f\n",new_gain);
 	return new_gain * SPEED;
 }
 
 void turn(int degrees){
-	int target_angle = orientation*90+degrees;
+	double target_angle = orientation*90+degrees;
+	if (target_angle == 360)             //workaround the modulus limitation on line 87 (fmod)
+		target_angle = 359.8;
+	if (target_angle == -360)
+		target_angle = -359.8;
     if (degrees>0)
 	    while (current_angle < target_angle){
 	    	angle_change();
+	    	printf("Target angle: %f | Current angle: %f\n",target_angle,current_angle);
 	        set_motors(new_speed(target_angle,current_angle),-new_speed(target_angle,current_angle));
-	        //set_motors(10,-10);
 	    }
 	else 
 	    while (current_angle > target_angle){
 	        angle_change();
+	        printf("Target angle: %f | Current angle: %f\n",target_angle,current_angle);
 	        set_motors(-new_speed(target_angle,current_angle),new_speed(target_angle,current_angle));
-	        //set_motors(-10,10);
 	    }
     set_motors(0,0);
 }
-
-/*   TURN FAILURES
-
-void turn(int degrees){
-    reset_motor_encoders();
-    int initialleft,initialright,left=0,right=0;
-    get_motor_encoders(&initialleft,&initialright);
-    int speed=SPEED;
-    if (degrees>0) 
-	    while ((left-right)/2<=(initialleft+initialright)/2+abs(degrees)){
-	        get_motor_encoders(&left,&right);
-	        angle_change();
-	        if ((initialleft+initialright)/2+degrees-(left-right)/2<=SLOW_DIST)
-	            	speed=new_speed2((initialleft+initialright)/2+degrees-(left-right)/2);
-	        set_motors(speed,-speed);
-	    }
-	else 
-		while ((right-left)/2<=(initialleft+initialright)/2+abs(degrees)){
-	        get_motor_encoders(&left,&right);
-	        angle_change();
-	        if ((initialleft+initialright)/2+degrees-(right-left)/2<=SLOW_DIST)   	
-	            	speed=new_speed2((initialleft+initialright)/2+degrees-(right-left)/2);
-	        set_motors(-speed,speed);
-	    }
-    set_motors(0,0);
-}
-
-// circumference = pi * diameter
-// length of robot = 22.5 cm
-// diameter of wheels = 9.5 cm
-// 2 * pi radians in one circle
-// 1146 ticks in a full circle
-// circumference for wheel is 70.6858347 cm
-
-double ticks_to_cm(int degrees_in_turn) {
-	// Need to turn 90degrees
-	int wheel_rounds = ((M_PI * ROBOT_WIDTH * degrees_in_turn) / 360) / WHEEL_DIAMETER; // each wheel goes this many rounds when making a 90 degree turn
-	int rounds_to_ticks = wheel_rounds * TICKS_IN_METER / (M_PI * ROBOT_WIDTH) *2;
-	return rounds_to_ticks;
-}*/
 
 void drive_first_unit() {
     int initialleft,initialright,left=0,right=0;
@@ -197,20 +157,69 @@ void drive_unit() {
 }
 //////////////////////////////////////////
 
+///////// Trace functions //////////
+void drive_to_target() {
+	set_point(target_point_x, target_point_y);
+	double heading_r_to_p = atan2(target_point_y - total_d_y, target_point_x - total_d_x);
+	angle_change();
+	double error = fmod(current_angle - heading_r_to_p + 2*M_PI, 2*M_PI);
+	if(error > M_PI) {
+		error -= 2 * M_PI;
+	}
+	int newtestspeedRight = 0;
+	int newtestspeedLeft = 0;
+	newtestspeedRight = (int)round(SPEED + (SPEED * error));
+	newtestspeedLeft = (int)round(SPEED - (SPEED * error));
+	set_motors(newtestspeedRight, newtestspeedLeft);
+}
+
+
+void trace() {
+	while (current->previous != NULL) {
+		if (hypotenuse(total_d_y - current ->y, total_d_x - current ->x) < 25.0) {
+			current = current -> previous;
+		}
+		else {
+			target_point_x = current -> x;
+			target_point_y = current -> y;
+			drive_to_target();
+		}
+	}
+	while(hypotenuse(total_d_y - current->y, total_d_x - current->x) > 1.0){
+		drive_to_target();
+	}
+}
+//////////////////////////////////////////
+
 ///////// Phase 1 //////////
-void mapper(int orientation) {
+void mapper(int orientation) {   //only updates position at this time
 	if (orientation==0) {
-		position=position+4;               
+		if (position == 0)
+			position=position+1;
+		else
+			position=position+4;               
 	}
 	if (orientation==1) {
 		position=position+1;
 	}
 	if (orientation==2) {
-		position=position-4;
+		if (position == 1)
+			position=position-1;
+		else
+			position=position-4;
 	}
 	if (orientation==3) {
 		position=position-1;
 	}	
+}
+
+void stopper() {
+	if (position == 0) {
+		if (stop == 0)
+			stop++;
+		else
+			exit(0);
+	}
 }
 
 /*void correction() {
@@ -259,62 +268,32 @@ void maze_explorer() {
 		get_readings();
 		//correction();
 		mapper(orientation);
+		stopper();
 	    //setLinkedList(total_d_x, total_d_y);
 
 	    if (sideLeftDist>30) {
+	    	printf("Turning -90\n");
 	    	turn(-90); 
+	    	printf("Finished Turning -90\n");
 	    	orientation=(orientation-1)%4;
-	    	printf("Turning -90\n");	
 	    }
-	    else if (USDist>30) {
-			printf("Moving ahead\n");
+	    else if (USDist<30) {
+	    	if (sideRightDist<30) {
+				printf("Turning 180\n");
+				turn(180);
+				printf("Finished Turning 180\n");
+				orientation=(orientation+2)%4;
+			}
+			else {
+				printf("Turning 90\n");
+				turn(90);
+				printf("Finished Turning 90\n");
+				orientation=(orientation+1)%4;
+			}
 		}
-		else if (sideRightDist<30) {
-			turn(180);
-			orientation=(orientation+2)%4;
-			printf("Turning 180\n");
-		}
-		else {
-			
-			turn(90);
-			orientation=(orientation+1)%4;
-			printf("Turning 90\n");
-		}
+		printf("Moving ahead\n");
 		drive_unit();
-	}
-}
-//////////////////////////////////////////
-
-///////// Trace functions //////////
-void drive_to_target() {
-	set_point(target_point_x, target_point_y);
-	double heading_r_to_p = atan2(target_point_y - total_d_y, target_point_x - total_d_x);
-	angle_change();
-	double error = fmod(current_angle - heading_r_to_p + 2*M_PI, 2*M_PI);
-	if(error > M_PI) {
-		error -= 2 * M_PI;
-	}
-	int newtestspeedRight = 0;
-	int newtestspeedLeft = 0;
-	newtestspeedRight = (int)round(SPEED + (SPEED * error));
-	newtestspeedLeft = (int)round(SPEED - (SPEED * error));
-	set_motors(newtestspeedRight, newtestspeedLeft);
-}
-
-
-void trace() {
-	while (current->previous != NULL) {
-		if (hypotenuse(total_d_y - current ->y, total_d_x - current ->x) < 25.0) {
-			current = current -> previous;
-		}
-		else {
-			target_point_x = current -> x;
-			target_point_y = current -> y;
-			drive_to_target();
-		}
-	}
-	while(hypotenuse(total_d_y - current->y, total_d_x - current->x) > 1.0){
-		drive_to_target();
+		printf("Finished Moving ahead\n");
 	}
 }
 //////////////////////////////////////////
